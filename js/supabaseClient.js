5227ea62de37ae22ec3dcf1d1d642dac8b1ff6e3
cse24-112 <cse24-112@thuto.bac.ac.bw>
2026-06-20 14:42:22 +0200

    Use Supabase anon key for public client

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://rlwdmctzzkibvdboeemy.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_2vrFLpQu4AlBTfuQ-KZZoA_mnk-Qwbk";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsd2RtY3R6emtpYnZkYm9lZW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5NDc0MzksImV4cCI6MjA5NjUyMzQzOX0.r9ADYuBTPPX-F9i6WQB6fX7z4DdC5U5IR4AFtAStCN0";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
